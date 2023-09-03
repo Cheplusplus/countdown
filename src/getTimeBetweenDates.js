e9a1f6739dbf0b98118d6102de346b160ed70842
CodeSpaceChe <che@codespace.co.za>
2023-09-04 01:01:07 +0200

    Completed

---
+++ src/getTimeBetweenDates.js
@@ -0,0 +1,25 @@
+/**
+ * 
+ * @param {Date} startDate 
+ * @param {Date} endDate 
+ * @returns {int[]}
+ * 
+ * Takes in 2 Date objects and returns the time between them as an array consisting of the days, hours, minutes and seconds.
+ */
+export const getTimeBetweenDates = (startDate, endDate) => {
+    let days = (endDate.getTime() - startDate.getTime()) / 1000 / 60 / 60 / 24;
+    let hours = days % Math.floor((days !== 0) ? 1 : days) * 24
+    let minutes = hours % Math.floor((hours !== 0) ? 1 : hours) * 60
+    let seconds = minutes % Math.floor((minutes !== 0) ? 1 : minutes) * 60
+  
+    return [
+        Math.floor(days).toLocaleString('en-US', {
+            minimumIntegerDigits: 2}),
+        Math.floor(hours).toLocaleString('en-US', {
+            minimumIntegerDigits: 2}),
+        Math.floor(minutes).toLocaleString('en-US', {
+            minimumIntegerDigits: 2}),
+        Math.floor(seconds).toLocaleString('en-US', {
+            minimumIntegerDigits: 2}),
+    ]
+  }
